391129bf48f2165ae85677d44a46d5f0c4042f43
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-24 05:32:30 -0800

    service

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import * as http from "http"
 console.log("starting the server...")
 
 http.createServer((request, response) => {
-  console.log(request)
+  console.log(request.method)
+  console.log(request.url)
   response.end()
 }).listen(8000)
 
