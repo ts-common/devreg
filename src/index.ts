a3079783127407661be89239fda1603868033d6d
Sergey Shandar <sergey.shandar@gmail.com>
2018-11-28 12:03:55 -0800

    li

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,116 @@
-import * as http from "http"
+import * as fs from "fs"
+import * as json from "@ts-common/json"
+import * as jsonParser from "@ts-common/json-parser"
+import * as path from "path"
+import * as it from "@ts-common/iterator"
+import * as sm from "@ts-common/string-map"
+import * as semver from "semver"
+import * as cp from "child_process"
 
-console.log("starting the server...")
+const nodeModules = "node_modules"
 
-http.createServer((request, response) => {
-  console.log(request.method)
-  console.log(request.url)
-  response.end()
-}).listen(8000)
+interface Dependencies {
+  readonly [name: string]: string|undefined
+}
 
-console.log("started.")
+interface PackageJson {
+  readonly name: string
+  readonly version: string
+  readonly dependencies?: Dependencies
+}
+
+const readPackageJson = (file: string): PackageJson =>
+  jsonParser.parse(file, fs.readFileSync(file).toString()) as PackageJson
+
+interface VersionLocation {
+  readonly version: string
+  readonly location: string
+}
+
+/* // use maxSatisfies() function.
+interface Versions {
+  readonly [version: string]: ReadonlyArray<string> | undefined
+}
+*/
+
+const packages = (p: string): Iterable<sm.Entry<VersionLocation>> =>
+  it.flatMap(
+    fs.readdirSync(p, { withFileTypes: true }),
+    d => {
+      if (!d.isDirectory()) {
+        return []
+      }
+      const dir = path.join(p, d.name)
+      const pj = path.join(dir, "package.json")
+      if (fs.existsSync(pj)) {
+        const j = readPackageJson(pj)
+        return [sm.entry(j.name, { version: j.version, location: dir })]
+      }
+      return packages(dir)
+    }
+  )
+
+const main = () => {
+  const current = path.resolve(".")
+
+  const packageJson = readPackageJson(path.join(current, "package.json"))
+
+  const dependencies = packageJson["dependencies"]
+
+  if (dependencies === undefined || !json.isObject(dependencies)) {
+    return
+  }
+
+  const localPackages = sm.stringMap(packages(path.join(current, "..", "..")))
+  // console.log(localPackages)
+
+  const p = sm.stringMap(packages(path.join(current, nodeModules)))
+  for (const [name, version] of sm.entries(dependencies)) {
+    const versionLocation = p[name]
+    if (versionLocation === undefined || !semver.satisfies(versionLocation.version, version)) {
+      console.log(`searching for ${name}@${version}`)
+      const versions = JSON.parse(cp.execSync(`npm view ${name} versions --json`).toString()) as
+        ReadonlyArray<string>
+      if (versions.find(v => semver.satisfies(v, version)) !== undefined) {
+        const x = cp.execSync(`npm install ${name}@${version} --no-save --package-lock-only`).toString()
+        console.log(x)
+      } else {
+        const local = localPackages[name]
+        if (local === undefined || !semver.satisfies(local.version, version)) {
+          console.log("not found")
+        } else {
+          const tgz = `${name.replace("@", "").replace("/", "-")}-${local.version}.tgz`
+          const pathTgz = path.join(local.location, tgz)
+          if (!fs.existsSync(pathTgz)) {
+            const o = cp.execSync(`npm pack`, { cwd: local.location }).toString()
+            console.log(o)
+          }
+          const ox = cp.execSync(`npm install ${pathTgz} --no-save --package-lock-only`).toString()
+          console.log(ox)
+        }
+      }
+    }
+  }
+
+  const f = cp.execSync("npm ci").toString()
+  console.log(f)
+
+  /*
+  const current = path.resolve(".")
+
+  // scan `node_modules`
+  const folders = fs.readdirSync(path.join(current, node_modules))
+
+  // scan folders for .tgz and packages. it may run 'pack' if there is no tgz.
+  // ...
+
+  for (const [packageId, version] of Object.entries(dependencies)) {
+  }
+
+  // run npm install ...tgz --no-save --package-lock-only
+  // ...
+  // run npm ci
+  */
+}
+
+main()
